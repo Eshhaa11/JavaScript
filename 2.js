037e229b93f1675e01707efbad435a84f4d3ac55
Eshhaa11 <patellesha11@gmail.com>
2025-01-22 13:47:43 +0300

    add Basic Functions

---
+++ 2.js
@@ -0,0 +1,11 @@
+//Basic multiplication 
+
+//Complete the function multiply such that it returns the product (the result of multiplication) of a and b.
+
+function multiply(a, b) {
+    return a * b;
+
+}
+
+console.log(multiply(2, 4));
+console.log(multiply(3, 2));
