191420b2c3071175481bc88c16cb23500abb5895
Eshhaa11 <patellesha11@gmail.com>
2025-01-22 13:58:26 +0300

    Add Strings I

--- Length.js
+++ Length.js
@@ -1,4 +1,10 @@
 //String length 
 
 //Complete the function getCharCount such that it returns the number of characters in the str parameter that it receives.
+function getCharCount(str) {
+   return str.length;
 
+}
+console.log(getCharCount("Sam")); // 3
+console.log(getCharCount("Alex 123")); // 8
+console.log(getCharCount("Charley is here")); // 15
